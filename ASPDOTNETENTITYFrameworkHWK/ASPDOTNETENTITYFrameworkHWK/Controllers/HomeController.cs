b49dce815ea09e6566cba95003c1484db4eda864
yolanda <ysimpson@spartaglobal.com>
2019-03-14 09:25:43 +0000

    databse connected, wont link to VS

--- ASPDOTNETENTITYFrameworkHWK/ASPDOTNETENTITYFrameworkHWK/Controllers/HomeController.cs
+++ ASPDOTNETENTITYFrameworkHWK/ASPDOTNETENTITYFrameworkHWK/Controllers/HomeController.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace ASPDOTNETENTITYFrameworkHWK.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            
             return View();
+            
         }
 
         public ActionResult About()
